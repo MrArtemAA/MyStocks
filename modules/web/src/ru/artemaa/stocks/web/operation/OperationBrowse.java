c9866a5fa1496458e50878632c6b649024b046d4
Artem Areshko <artemaa@gmail.com>
2018-03-07 12:12:32 +0300

    Добавлено массовое изменение операций

--- modules/web/src/ru/artemaa/stocks/web/operation/OperationBrowse.java
+++ modules/web/src/ru/artemaa/stocks/web/operation/OperationBrowse.java
@@ -1,6 +1,23 @@
 package ru.artemaa.stocks.web.operation;
 
 import com.haulmont.cuba.gui.components.AbstractLookup;
+import com.haulmont.cuba.gui.components.actions.EditAction;
+
+import javax.inject.Named;
+import java.util.Map;
+
+import static com.haulmont.cuba.gui.WindowManager.OpenType.DIALOG;
 
 public class OperationBrowse extends AbstractLookup {
+    @Named("operationsTable.edit")
+    EditAction editAction;
+
+    @Override
+    public void init(Map<String, Object> params) {
+        super.init(params);
+
+        editAction.getBulkEditorIntegration()
+                .setEnabled(true)
+                .setOpenType(DIALOG);
+    }
 }
