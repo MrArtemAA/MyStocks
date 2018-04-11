68e0a3383f53bcc85625b4417b71aa9994bfa8ae
Artem Areshko <artemaa@gmail.com>
2018-04-11 20:43:09 +0300

    Переводы между счетами с разными валютами

--- modules/web/src/ru/artemaa/stocks/web/transfer/TransferEdit.java
+++ modules/web/src/ru/artemaa/stocks/web/transfer/TransferEdit.java
@@ -3,10 +3,13 @@ package ru.artemaa.stocks.web.transfer;
 import com.haulmont.cuba.gui.components.AbstractEditor;
 import com.haulmont.cuba.gui.components.LookupPickerField;
 import com.haulmont.cuba.gui.components.TextField;
+import ru.artemaa.stocks.entity.Account;
 import ru.artemaa.stocks.entity.operation.Transfer;
 
+import javax.inject.Inject;
 import javax.inject.Named;
 import java.util.Map;
+import java.util.Objects;
 
 public class TransferEdit extends AbstractEditor<Transfer> {
     @Named("fieldGroup.sourceAccount")
@@ -15,23 +18,37 @@ public class TransferEdit extends AbstractEditor<Transfer> {
     private LookupPickerField destAccountField;
     @Named("fieldGroup.rate")
     private TextField rateField;
+    @Inject
+    private TextField destSum;
 
     @Override
     public void init(Map<String, Object> params) {
         super.init(params);
 
         rateField.setVisible(false);
+        destSum.setVisible(false);
     }
 
     @Override
     protected void postInit() {
         super.postInit();
 
-        /*sourceAccountField.addValueChangeListener(e -> {
+        sourceAccountField.addValueChangeListener(e -> {
             Account account = (Account) e.getValue();
-            if (Objects.nonNull(account) && Objects.nonNull(getItem().getDestAccount())) {
-
+            Account destAccount = getItem().getDestAccount();
+            if (Objects.nonNull(account) && Objects.nonNull(destAccount)) {
+                if (isDifferentCurrencies(account, destAccount)) {
+                    rateField.setVisible(true);
+                    destSum.setVisible(true);
+                } else {
+                    rateField.setVisible(false);
+                    destSum.setVisible(false);
+                }
             }
-        });*/
+        });
+    }
+
+    private boolean isDifferentCurrencies(Account account1, Account account2) {
+        return !account1.getCurrencyCode().equals(account2.getCurrencyCode());
     }
 }
