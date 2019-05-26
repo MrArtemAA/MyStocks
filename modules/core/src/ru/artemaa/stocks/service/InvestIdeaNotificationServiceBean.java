ebb901e1b12743de6ec7abbdd5456522a43217c7
Artem Areshko <artemaa@gmail.com>
2019-05-26 18:44:57 +0300

    Added template, when there are no invest ideas

--- modules/core/src/ru/artemaa/stocks/service/InvestIdeaNotificationServiceBean.java
+++ modules/core/src/ru/artemaa/stocks/service/InvestIdeaNotificationServiceBean.java
@@ -30,21 +30,36 @@ public class InvestIdeaNotificationServiceBean implements InvestIdeaNotification
         List<InvestIdea> investIdeas = dataManager.loadList(LoadContext.create(InvestIdea.class).setQuery(
                 LoadContext.createQuery("select e from stocks$InvestIdea e " +
                         "where e.active = TRUE and e.endDate <= :endDate")
-                .setParameter("endDate", new Date())
+                        .setParameter("endDate", new Date())
         ));
 
+        emailerAPI.sendEmailAsync(getEmailInfo());
+    }
+
+    private EmailInfo getEmailInfo() {
         String userEmail = stocksConfig.getUserEmail();
         if (StringUtils.isEmpty(userEmail)) {
             throw new RuntimeException("В свойствах базы данных не указан email пользователя для отправки уведомления");
         }
+
         Map<String, Serializable> templateParams = new HashMap<>();
-        templateParams.put("investIdeas", (Serializable) investIdeas);
-        emailerAPI.sendEmailAsync(new EmailInfo(
-                userEmail,
-                "Доброе утро! Ваша информация по текущим инвестиционным идеям",
-                null,
-                "ru/artemaa/stocks/mailtemplates/invest-ideas-summary.html",
-                templateParams
-        ));
+        if (CollectionUtils.isEmpty(investIdeas)) {
+            return new EmailInfo(
+                    userEmail,
+                    "Инвестидей на сегодня нет",
+                    null,
+                    "ru/artemaa/stocks/mailtemplates/no-invest-ideas.html",
+                    templateParams);
+
+        } else {
+
+            templateParams.put("investIdeas", (Serializable) investIdeas);
+            return new EmailInfo(
+                    userEmail,
+                    "Ваша информация по текущим инвестиционным идеям",
+                    null,
+                    "ru/artemaa/stocks/mailtemplates/invest-ideas-summary.html",
+                    templateParams);
+        }
     }
 }
